59a5cb6a8561f238aa4293ef58ece4f22f187a94
Елизавета <liza@tune-it.ru>
2021-01-22 15:21:31 +0300

    delete currentflights

--- src/app/client/main/main.component.ts
+++ src/app/client/main/main.component.ts
@@ -15,19 +15,13 @@ export class MainComponent implements OnInit {
   public flight: Flight = new Flight();
   public flight1: Flight = new Flight();
   public allflights: Flight[] = [];
-  public currentflights: Flight[] = [];
   public puncts: string[] = [];
 
   public bookMode = false;
   ngOnInit() {
-   // this.flightService.getPuncts().subscribe(data => (data as string[]).forEach( p => this.puncts.push(p)));
+    this.flightService.getPuncts().subscribe(data => (data as string[]).forEach( p => this.puncts.push(p)));
     this.allflights.push(this.flight);
     this.allflights.push(this.flight1);
-    this.currentflights = this.allflights;
-    this.puncts.push('kjk');
-    this.puncts.push('njgfgkj');
-    this.puncts.push('kjfk');
-    this.puncts.push('njkj');
   }
   searchFlights(dep: string, arr: string, date: string, count: string) {
 this.flightService.getFlights(dep, arr, new Date(date), Number(count))
@@ -40,25 +34,4 @@ this.bookMode = false;
   loadBook(val: string) {
     this.bookService.sendBookID(val);
   }
-
-  //   public filterbydate(date: string) {
-  //   this.currentflights = this.allflights.filter(flight => flight.arrtime.toLocaleDateString() === new Date(date).toLocaleDateString());
-  //   }
-  //
-  //   public filterbydep(dep: string) {
-  //   if (dep === 'Не выбрано') { this.currentflights = this.allflights; return; }
-  //   this.currentflights = this.allflights.filter(flight => flight.dep === dep);
-  //   }
-  //
-  // public filterbyarr(arr: string) {
-  //   if (arr === 'Не выбрано') { this.currentflights = this.allflights; return; }
-  //   this.currentflights = this.allflights.filter(flight => flight.arr === arr);
-  // }
-  // public filterbycount(count: string) {
-  //   if (count === '') { this.currentflights = this.allflights; return; }
-  //   this.currentflights = this.allflights.filter(flight => flight.count === Number(count));
-  // }
-  // public filter(date:string,dep:string,arr:string,count:string){
-  // }
-
 }
