ce3495f14123eb797c4a662433e7af2a97a782e2
whatislove118 <oleg.pochikalin@mail.ru>
2021-01-24 19:15:27 +0300

    admin_aircraft_search

--- src/app/model/aircraft.ts
+++ src/app/model/aircraft.ts
@@ -1,12 +1,12 @@
 export class Aircraft {
    company: string;
-  id: number;
+  id: string;
   location: string;
   aircraftmodel: string;
 
   constructor(company: string, id: string, location: string, aircraftmodel: string) {
     this.company = company;
-    this.id = Number(id);
+    this.id = id;
     this.location = location;
     this.aircraftmodel = aircraftmodel;
   }
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -6,6 +6,7 @@ import {Aircraft} from '../model/aircraft';
 import {Company} from '../model/company';
 import {Flight} from '../model/flight';
 import {WorkAtTime} from '../model/work-at-time';
+import {AppComponent} from '../app.component';
 
 @Injectable({
   providedIn: 'root'
@@ -22,22 +23,22 @@ export class AdminService {
     return headers;
   }
   getPuncts() {
-    return this.httpClient.get('gfg', {headers: AdminService.getHeaders()});
+    return this.httpClient.get(AppComponent.API_URL + 'flight/search/direction/', {headers: AdminService.getHeaders()});
   }
   getAircrafts() {
-   return  this.httpClient.get('url', {headers: AdminService.getHeaders()});
+   return  this.httpClient.get(AppComponent.API_URL + 'admin/aircraft/get/', {headers: AdminService.getHeaders()});
   }
   getCompanies() {
-   return  this.httpClient.get('url', {headers: AdminService.getHeaders()});
+   return  this.httpClient.get(AppComponent.API_URL + 'admin/companies/get/', {headers: AdminService.getHeaders()});
   }
   getEmployers() {
-    return this.httpClient.get('url', {headers: AdminService.getHeaders()});
+    return this.httpClient.get(AppComponent.API_URL + 'admin/employee/get/', {headers: AdminService.getHeaders()});
   }
   getFlights() {
-   return  this.httpClient.get('url', {headers: AdminService.getHeaders()});
+   return  this.httpClient.get(AppComponent.API_URL  + 'admin/flight/get/', {headers: AdminService.getHeaders()});
   }
   getSchedule(passport: number) {
-   return  this.httpClient.get('url', {headers: AdminService.getHeaders()});
+   return  this.httpClient.get(AppComponent.API_URL  + 'admin/schedule/get/', {headers: AdminService.getHeaders()});
   }
   addNewAircraft(name: string, mod: string, comp: string) {
     const body = {
@@ -45,7 +46,7 @@ export class AdminService {
       model: mod,
       company: comp
     };
-    return  this.httpClient.post('url', body, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL  + 'admin/aircraft/add/', body, {headers: AdminService.getHeaders()});
   }
   addNewEmpl(pass: string, n: string, s: string, p: string, pos: string, comp: string) {
     const body = {
@@ -56,7 +57,7 @@ export class AdminService {
       position: pos,
       company: comp
     };
-    return  this.httpClient.post('url', body, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL  + 'admin/employee/add/', body, {headers: AdminService.getHeaders()});
   }
   addNewFlight(air: string, depT: Date, arrT: Date, dep: string, arr: string) {
     const body = {
@@ -66,7 +67,7 @@ export class AdminService {
       departure_airport: dep,
       arrival_airport: arr
     };
-    return  this.httpClient.post('url', body, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/flight/add/', body, {headers: AdminService.getHeaders()});
   }
   addNewSchedule(empl: number, flt: string, start: Date, finish: Date, gt: string) {
     const body = {
@@ -76,41 +77,41 @@ export class AdminService {
       finish_time: finish,
       gate: gt
     };
-    return  this.httpClient.post('url', body, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL  + 'admin/schedule/add/', body, {headers: AdminService.getHeaders()});
   }
   addNewCompany(nm: string, t: string) {
     const body = {
       name: nm,
       type: t
     };
-    return  this.httpClient.post('url', body, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/companies/add/', body, {headers: AdminService.getHeaders()});
   }
   deleteAircraft(aircraft: string) {
-   return  this.httpClient.post('url', {id: aircraft}, {headers: AdminService.getHeaders()});
+   return  this.httpClient.post(AppComponent.API_URL + 'admin/aircraft/delete/', {id: aircraft}, {headers: AdminService.getHeaders()});
   }
   changeAircraft(aircraft: Aircraft) {
-   return  this.httpClient.post('url', {owner_id: aircraft.company, model: aircraft.aircraftmodel}, {headers: AdminService.getHeaders()});
+   return  this.httpClient.post(AppComponent.API_URL + 'admin/aircraft/change/', {owner_id: aircraft.company, model: aircraft.aircraftmodel}, {headers: AdminService.getHeaders()});
   }
   deleteCompany(comp: Company) {
-    return  this.httpClient.post('url', {name: comp.name, type: comp.type}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/companies/delete/', {name: comp.name, type: comp.type}, {headers: AdminService.getHeaders()});
   }
   deleteEmpl(pass: number) {
-    return  this.httpClient.post('url', {passport: pass}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/employee/delete/', {passport: pass}, {headers: AdminService.getHeaders()});
   }
   changeEmployee(empl: Employer) {
-    return  this.httpClient.post('url', {employee: empl}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/employee/change/', {employee: empl}, {headers: AdminService.getHeaders()});
   }
   deleteFlight(flight: number) {
-    return  this.httpClient.post('url', {id: flight}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/employee/delete/', {id: flight}, {headers: AdminService.getHeaders()});
   }
   changeFlight(flt: Flight) {
-    return  this.httpClient.post('url', {flight: flt}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/flight/change/', {flight: flt}, {headers: AdminService.getHeaders()});
   }
   deleteWork(flt: number, empl: number) {
-    return  this.httpClient.post('url', {flight: flt, employee: empl}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/schedule/delete/', {flight: flt, employee: empl}, {headers: AdminService.getHeaders()});
   }
   changeWork(wrk: WorkAtTime, empl: number) {
-    return  this.httpClient.post('url', {work: wrk, employee: empl}, {headers: AdminService.getHeaders()});
+    return  this.httpClient.post(AppComponent.API_URL + 'admin/schedule/change/', {work: wrk, employee: empl}, {headers: AdminService.getHeaders()});
   }
 
 
